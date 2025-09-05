fb4954b5f39e7e4334b1fc6ecf0efd2cb4ec5eba
samiyjawabrah <samiyjawabrah@gmail.com>
2025-09-04 22:16:27 -0400

    Created first working tunning for resnet18: there are indicatyions of overfitting from the results: Epoch 1/5 - Training Loss 0.7253 Validation Loss: 0.5269, Accuracy: 81.05% Epoch 2/5 - Training Loss 0.1706 Validation Loss: 0.4984, Accuracy: 81.24% Epoch 3/5 - Training Loss 0.0461 Validation Loss: 0.5427, Accuracy: 82.00% Epoch 4/5 - Training Loss 0.0265 Validation Loss: 0.5384, Accuracy: 82.57% Epoch 5/5 - Training Loss 0.0156 Validation Loss: 0.5744, Accuracy: 82.57%
    Good Luck Future Sami!

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -15,13 +15,19 @@ LR = 0.0001
 EPOCHS = 5
 data_dir = '../data/House_Room_Dataset/'
 
+# test_transform = transforms.Compose([
+  #   transforms.Resize((IMG_HEIGHT, IMG_WIDTH)),
+  #   transforms.ToTensor(),
+  #   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+  # ])
+
 def main():
 
   dataset = load_data(data_dir)
   train_data, val_data = train_test_split(dataset, train_size=0.8, test_size=0.2) 
 
   train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
-  val_dataloader = DataLoader(val_data, batch_size=32, shuffle=False)
+  val_dataloader = DataLoader(val_data, batch_size=32, shuffle=False) #Shuffle should be false for validation set
   
   model =  models.resnet18(pretrained=True)
   num_features = model.fc.in_features
@@ -33,29 +39,49 @@ def main():
   train_losses = []
   
   for epoch in range(EPOCHS):
+    train_loss = 0
     for images, labels in train_dataloader:
         
-        outputs = model(images)
-        loss = criterion(outputs,labels)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-      
-    print(f"Completed Epoch {epoch+1}/{EPOCHS}")
+      outputs = model(images)
+      loss = criterion(outputs,labels)
+      train_loss += loss.item() #Adds the numeric loss, must have the .item()
+      loss.backward()
+      optimizer.step()
+      optimizer.zero_grad()
+    
+    avg_train_loss = train_loss / len(train_dataloader)
+    train_losses.append(avg_train_loss)
+    print(f"Epoch {epoch+1}/{EPOCHS} - Training Loss {avg_train_loss:.4f}")
 
-  test_transform = transforms.Compose([
-    transforms.Resize((IMG_HEIGHT, IMG_WIDTH)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-  ])
-      
-  return
+    val_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+      for images, labels in val_dataloader:
+        
+        val_output = model(images)
+        loss = criterion(val_output, labels)
+        val_loss += loss.item()
+
+        _, predicted = val_output.max(1)
+        correct += (predicted == labels).sum().item()
+        total += labels.size(0)
+    avg_val_loss = val_loss / len(val_dataloader)
+    val_accuracy = correct / total * 100
+    print(f"Validation Loss: {avg_val_loss:.4f}, Accuracy: {val_accuracy:.2f}%")
+
+  if not os.path.exists("models"):
+    os.makedirs("models")
+
+  torch.save(model.state_dict(), 'models/house_room_model.pth')
+
+  return model, train_losses
 
 
 def load_data(data_dir):
 
   train_transform = transforms.Compose([
-    transforms.RandomVerticalFlip(15),
+    transforms.RandomVerticalFlip(0.5),
     transforms.ColorJitter(brightness=0.2),
     transforms.Resize((IMG_HEIGHT, IMG_WIDTH)),
     transforms.ToTensor(),
@@ -75,11 +101,11 @@ def load_data(data_dir):
               labels.append(image_dict[filename])
           else:
                 continue
-
-  return (images,labels)
+  dataset = list(zip(images,labels))
+  return dataset
 
 
 if __name__ == '__main__':
-    # main()
-    images = load_data('../data/House_Room_Dataset/')
-    print(images[0][0].shape)
+    main()
+    # images = load_data('../data/House_Room_Dataset/')
+    # print(images[0][0].shape)
